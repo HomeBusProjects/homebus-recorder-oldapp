41df0b144d64073de462567126028a14a695d1dd
John Romkey <58883+romkey@users.noreply.github.com>
2019-05-17 19:47:43 -0700

    send backup output to the right place

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-set :output, 'log/whenever.log'
+set :output, "~/recorder/shared/log/whenever.log"
 
 every 1.hour do
   command "cd ~/recorder/current/backup ; bundle exec backup -t hourly_backup.rb"
