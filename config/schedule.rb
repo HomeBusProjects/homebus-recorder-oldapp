9994bcf059b7ecd41618077867bf28e8f65a87d3
John Romkey <58883+romkey@users.noreply.github.com>
2019-05-17 19:45:25 -0700

    try to fix whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, 'log/whenever.log'
 
 every 1.hour do
-  system "cd backup ; bundle exec backup -t hourly_backup.rb"
+  command "cd ~/recorder/current/backup ; bundle exec backup -t hourly_backup.rb"
 end
