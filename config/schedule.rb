a1665c5d13f6f053337bfbbf6b9578173f88de7e
John Romkey <58883+romkey@users.noreply.github.com>
2019-05-17 20:12:56 -0700

    fix the backup command

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 set :output, "~/recorder/shared/log/whenever.log"
 
 every 1.hour do
-  command "cd ~/recorder/current/backup ; bundle exec backup -t hourly_backup.rb"
+  command "cd ~/recorder/current/backup ; bundle exec backup perform -t hourly_backup.rb"
 end
