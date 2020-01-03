b207d50b0b359d22ef67fdc3dc0faa9e3ab7b4eb
John Romkey <58883+romkey@users.noreply.github.com>
2020-01-02 20:47:30 -0800

    added reports to routes
    got rid of superfluous lookups in report controller

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,11 +1,5 @@
 class ReportController < ApplicationController
   def index
-    @access = Device.access
-    @cameras = Device.camera
-    @discoballs = Device.discoball
-    @furballs = Device.furball
-    @printers_3d = Device.printers_3d
-
     @network_active_hosts_max = Sample.where('created_at > ?', Time.now - 1.day).maximum("((data->'active_hosts'->>'arp_table_length')::integer)")
 
     Sample.where(uuid: '638a9d78-09f6-4d92-b70a-2b14edb8d84a').where('created_at > ?', Time.now - 1.day).maximum("((data->'environment'->>'temperature')::real)")
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :samples
   resources :create_device_tables
   get 'dashboard', to: 'dashboard#index'
+  get 'reports', to: 'report#index'
   get 'samples_clear', to: 'samples#clear'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
