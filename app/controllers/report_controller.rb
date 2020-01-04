582188b36b08cbbb28a98b8a8e9914c8fced80a5
John Romkey <58883+romkey@users.noreply.github.com>
2020-01-04 08:13:14 -0800

    added weather stations

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -16,6 +16,10 @@ class ReportController < ApplicationController
         name: 'Unit 2'
       },
       {
+        uuid: '1955b27e-17fd-429a-b1ce-293879149e42',
+        name: 'Unit 2 (weather station)'
+      },
+      {
         uuid: '067746e8-7a2f-4f6e-bb59-e1b2043b37f3',
         name: 'Slytherin'
       },
@@ -31,7 +35,7 @@ class ReportController < ApplicationController
       }
     ]
 
-    printers = [
+    @printers = [
       {
         uuid: '7bfff065-f2e8-406d-b8d1-734a84c4c222',
         name: 'Dremel'
@@ -47,14 +51,24 @@ class ReportController < ApplicationController
       }
     ]; 
 
-    servers = [
-                '8cb9e03e-1bfe-4ad7-b96e-e9186956b746',
-                '59905e56-b683-4b22-bc66-345705a3f27e',
-                '9fb914b1-1c5a-40b0-8c6d-eac29bb1ea3d',
-                '0d1fd497-8cf7-47b2-b0fc-baab52c52301',
-                '7f9ee46e-355e-4b7c-a2c1-fe24e733796a',
-                '0b75a4f6-0467-4986-9b05-fedeba1bbbf8'
-            ]; 
+    @weather_stations = [
+      {
+        uuid: '691faad6-bad4-4c87-838b-091ce650e4d4',
+        name: 'OpenWeatherMap'
+      }, {
+        uuid: 'ea52198c-6162-4ea8-9e19-2f99bcfb4462',
+        name: 'CTRLH'
+      }
+    ]
+
+    @servers = [
+      '8cb9e03e-1bfe-4ad7-b96e-e9186956b746',
+      '59905e56-b683-4b22-bc66-345705a3f27e',
+      '9fb914b1-1c5a-40b0-8c6d-eac29bb1ea3d',
+      '0d1fd497-8cf7-47b2-b0fc-baab52c52301',
+      '7f9ee46e-355e-4b7c-a2c1-fe24e733796a',
+      '0b75a4f6-0467-4986-9b05-fedeba1bbbf8'
+    ]; 
 
 
     @network_active_hosts_max = Sample.where('created_at > ?', Time.now - 1.day).maximum("((data->'active_hosts'->>'arp_table_length')::integer)")
@@ -63,6 +77,14 @@ class ReportController < ApplicationController
       furball[:max_temp] = Sample.where(uuid: furball[:uuid]).where('created_at > ?', Time.now - 1.day).maximum("((data->'environment'->>'temperature')::real)")
       furball[:min_temp] = Sample.where(uuid: furball[:uuid]).where('created_at > ?', Time.now - 1.day).minimum("((data->'environment'->>'temperature')::real)")
       furball[:samples] = Sample.where(uuid: furball[:uuid]).where('created_at > ?', Time.now - 1.day).count
+    end
+
+    @weather_stations.each do |station|
+      station[:max_temp] = Sample.where(uuid: station[:uuid]).where('created_at > ?', Time.now - 1.day).maximum("((data->'environment'->>'temperature')::real)")
+      station[:min_temp] = Sample.where(uuid: station[:uuid]).where('created_at > ?', Time.now - 1.day).minimum("((data->'environment'->>'temperature')::real)")
+      station[:max_humidity] = Sample.where(uuid: station[:uuid]).where('created_at > ?', Time.now - 1.day).maximum("((data->'environment'->>'humidity')::real)")
+      station[:min_humidity] = Sample.where(uuid: station[:uuid]).where('created_at > ?', Time.now - 1.day).minimum("((data->'environment'->>'humidity')::real)")
+      station[:samples] = Sample.where(uuid: station[:uuid]).where('created_at > ?', Time.now - 1.day).count
     end
 
     @minutes_in_interval = (Time.now - 1.day).to_i/60
