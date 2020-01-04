501abb49db09ffe825ea7c18922702bbd6307eba
John Romkey <58883+romkey@users.noreply.github.com>
2020-01-03 21:25:06 -0800

    changed /reports to /report
    added report to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   resources :attributes
   resources :samples
   resources :create_device_tables
+
   get 'dashboard', to: 'dashboard#index'
-  get 'reports', to: 'report#index'
+
+  get 'report', to: 'report#index'
+
   get 'samples_clear', to: 'samples#clear'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
